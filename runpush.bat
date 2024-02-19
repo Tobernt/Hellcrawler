e2690227e6d61e819dd3f103bb8d05f5cddc3d82
Tobernt <tobias_berntsson@hotmail.com>
2024-02-19 12:48:14 +0100

    Added bat file to commit for us

--- runpush.bat
+++ runpush.bat
@@ -3,6 +3,6 @@ git init
 git remote add origin https://github.com/Tobernt/Hellcrawler.git
 git add .
 git status
-git commit -m "Your text here"
+git commit -m "Added bat file to commit for us"
 git push
 cmd /k 
