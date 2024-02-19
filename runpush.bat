db379c5701ae9f4fc9e95c455a38c5cd242c442b
LuxRegina <erika.s.hedstrom@gmail.com>
2024-02-19 15:43:08 +0100

    Added Background picture

--- runpush.bat
+++ runpush.bat
@@ -1,8 +1,9 @@
 %cd%
+set /p Input=Enter commit comment:
 git init
 git remote add origin https://github.com/Tobernt/Hellcrawler.git
 git add .
 git status
-git commit -m "Added bat file to commit for us"
+git commit -m "%Input%"
 git push
-cmd /k 
+cmd /k
