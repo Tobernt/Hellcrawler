b2c9fba8b7fd3f59cefb11f955f2eb00a1a53214
Tobernt <tobias_berntsson@hotmail.com>
2024-02-20 14:46:02 +0100

    Adjusted spawner and speed

--- Assets/Scripts/InitialSpawn.cs
+++ Assets/Scripts/InitialSpawn.cs
@@ -2,26 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SpawnPlatform : MonoBehaviour
+public class InitialSpawn : MonoBehaviour
 {
 
     public GameObject Platform;
-    public float spawnTime = 2f;
 
     // Start is called before the first frame update
-        void Start()
+    void Start()
     {
-        InvokeRepeating("spawnPlatform", spawnTime, spawnTime);
         Vector3 randomSpawnPosition = new Vector3(Random.Range(-1f, 1f), this.transform.position.y, 0);
         Instantiate(Platform, randomSpawnPosition, Quaternion.identity);
+        Destroy(this.gameObject);
     }
     // Update is called once per frame
-
-    void spawnPlatform()
-    {
-        Vector3 randomSpawnPosition = new Vector3(Random.Range(-1f, 1f), this.transform.position.y, 0);
-        Instantiate(Platform, randomSpawnPosition, Quaternion.identity);
-    }
 
     void Update()
     {
