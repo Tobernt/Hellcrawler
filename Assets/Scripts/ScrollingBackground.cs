49a85a434ef889a26c7e64ac5c05ffa7a14d18a3
LuxRegina <121197393+LuxRegina@users.noreply.github.com>
2024-02-20 12:54:57 +0100

    Scrolling background fixed

--- Assets/Scripts/ScrollingBackground.cs
+++ Assets/Scripts/ScrollingBackground.cs
@@ -2,17 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class LoopingBackground : MonoBehaviour
+public class ScrollingBackground : MonoBehaviour
 {
+
+    public float speed;
+
+    [SerializeField]
+    private Renderer backgroundRenderer;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        backgroundRenderer.material.mainTextureOffset += new Vector2(0, speed * Time.deltaTime);
     }
 }
