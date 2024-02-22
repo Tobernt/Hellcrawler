8cc6c289adf38206ce7afb2d1e589653673d5098
Christoffer Lundberg <148043316+Obaoba90@users.noreply.github.com>
2024-02-22 14:56:44 +0100

    SpaceLevel

---
+++ Assets/Scripts/SpaceSpawnPlatform.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpaceSpawnPlatform : MonoBehaviour
+{
+
+    public GameObject spaceSpawnPlatform;
+    private float spaceSpawnTime = 5f;
+
+    // Start is called before the first frame update
+        void Start()
+    {
+        InvokeRepeating("spawnPlatform", spaceSpawnTime, spaceSpawnTime);
+        Vector3 randomSpawnPosition = new Vector3(Random.Range(-2f, 2f), this.transform.position.y, 0);
+        Instantiate(spaceSpawnPlatform, randomSpawnPosition, Quaternion.identity);
+    }
+    // Update is called once per frame
+
+    void SpacespawnPlatform()
+    {
+        Vector3 randomSpaceSpawnPosition = new Vector3(Random.Range(-2f, 2f), this.transform.position.y, 0);
+        Instantiate(spaceSpawnPlatform, randomSpaceSpawnPosition, Quaternion.identity);
+        spaceSpawnTime = 0;
+    }
+
+    void Update()
+    {
+        spaceSpawnTime += Time.deltaTime;
+    }
+}
