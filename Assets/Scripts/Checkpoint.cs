da0052183834b4c43a249d3a8eb0e95c8d7d2441
LuxRegina <121197393+LuxRegina@users.noreply.github.com>
2024-02-26 11:57:23 +0100

    Added Checkpoints

--- Assets/Scripts/Checkpoint.cs
+++ Assets/Scripts/Checkpoint.cs
@@ -1,29 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Checkpoint : MonoBehaviour
 {
-    public GameObject checkpointPlatform;
-    public Transform spawnPoint;
-    private float spawnYPosition = 5f;
+    public int sceneBuildIndex;
 
-    private bool hasSpawned = false;
-
-    void checkpoint()
+    private void OnTriggerEnter2D(Collider2D other)
     {
-        if (Camera.main.transform.position.y >= spawnYPosition && !hasSpawned)
+        if(other.tag == "Player")
         {
-            Vector3 spawnPoint = new Vector3(spawnYPosition, this.transform.position.y);
-            // Spawn the object at the specified spawn point
-            Instantiate(checkpointPlatform, spawnPoint, Quaternion.identity);
-            hasSpawned = true; // Set the flag to prevent spawning multiple times
+            SceneManager.LoadScene(sceneBuildIndex, LoadSceneMode.Single); 
         }
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        checkpoint();
     }
 }
