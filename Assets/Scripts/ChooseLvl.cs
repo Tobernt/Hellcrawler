4cc43da057e1a56ece52da7e163f64b1881fc007
Christoffer Lundberg <148043316+Obaoba90@users.noreply.github.com>
2024-02-28 09:51:28 +0100

    ChooseLvl Button fix

--- Assets/Scripts/ChooseLvl.cs
+++ Assets/Scripts/ChooseLvl.cs
@@ -1,21 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
-using UnityEngine.UI;
 using UnityEngine.SceneManagement;
-
+using UnityEngine.UI;
 public class ChooseLvl : MonoBehaviour
 {
-    public int levelIndex; // Håller reda på vilken nivå knappen representerar
-
-    void Start()
+    public void Level1()
     {
-        // Hämta referensen till knappen och lägg till en lyssnare för dess klickhändelse
-        Button button = GetComponent<Button>();
-        button.onClick.AddListener(OnClick);
+        SceneManager.LoadScene(1);
     }
-
-    public void OnClick()
+    public void Levlel2()
     {
-        // Ladda den specifika nivån när knappen klickas
-        SceneManager.LoadScene(levelIndex);
+        SceneManager.LoadScene(2);
+    }
+    public void Level3()
+    {
+        SceneManager.LoadScene(3);
+    }
+    public void Level4()
+    {
+        SceneManager.LoadScene(4);
+    }
+    public void Level5()
+    {
+        SceneManager.LoadScene(5);
+    }
+    public void Level6()
+    {
+        SceneManager.LoadScene(6);
+    }
+    public void Level7()
+    {
+        SceneManager.LoadScene(7);
+    }
+    public void MainMenu()
+    {
+        SceneManager.LoadScene(0);
     }
 }
